5199d9d32c36a4ae61b0a016285c93b558c390f3
Zach Toolson <zach.toolson@gmail.com>
2014-05-21 18:38:46 -0600

    level 5 finished

--- level1.rb
+++
@@ -1,6 +0,0 @@
-class Player
-  def play_turn(warrior)
-    # add your code here
-    warrior.walk!
-  end
-end
--- level2.rb
+++
@@ -1,9 +0,0 @@
-class Player
-  def play_turn(warrior)
-    if warrior.feel.empty?
-      warrior.walk!
-    else
-      warrior.attack!
-    end
-  end
-end
