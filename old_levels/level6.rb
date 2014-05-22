f80caebd9811422e111428c32020893877589d96
Zach Toolson <zach.toolson@gmail.com>
2014-05-21 19:04:41 -0600

    level 6 completed in 2 ways

---
+++ old_levels/level6.rb
@@ -0,0 +1,39 @@
+class Player
+
+  def play_turn(warrior)
+
+    if taking_damage?(warrior) and warrior.health < 11
+      retreat(warrior)
+    elsif taking_damage?(warrior)
+      charge(warrior)
+    elsif warrior.feel.empty? and warrior.health < 20
+      warrior.rest!
+    else
+      charge(warrior)
+    end
+
+    @health = warrior.health
+  end
+
+  def charge(warrior)
+    if warrior.feel.empty? # nothing ahead, go ahead and walk
+      warrior.walk!
+    elsif warrior.feel.captive?
+      warrior.rescue!
+    else
+      warrior.attack!
+    end
+  end
+
+  def taking_damage?(warrior)
+    if @health
+      warrior.health < @health
+    else
+      false
+    end
+  end
+
+  def retreat(warrior)
+    warrior.walk!(:backward)
+  end
+end
